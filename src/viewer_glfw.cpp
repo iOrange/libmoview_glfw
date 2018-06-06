4ad829164d6759db0b9c81618af7ea7d86e0e520
iOrange <sergey.kudlay@hotmail.com>
2018-06-05 20:53:44 -0700

    * reworked UI

--- src/viewer_glfw.cpp
+++ src/viewer_glfw.cpp
@@ -23,7 +23,6 @@ struct UI {
     bool        showNormal;
     bool        showWireframe;
     bool        shouldExit;
-    std::string fileToOpen;
 };
 
 
@@ -35,40 +34,117 @@ inline float GetCurrentTimeSeconds() {
     return static_cast<float>(c) / static_cast<float>(CLOCKS_PER_SEC);
 }
 
-void DoUI(UI& ui, Composition* composition) {
+std::string     gMovieFilePath;
+std::string     gLicenseHash;
+bool            gToLoopPlay = false;
+Movie           gMovie;
+Composition*    gComposition = nullptr;
+
+void ShutdownMovie() {
+    if (gComposition) {
+        gMovie.CloseComposition(gComposition);
+        gComposition = nullptr;
+    }
+
+    gMovie.Close();
+    ResourcesManager::Instance().Shutdown();
+}
+
+bool ReloadMovie(const std::string& filePath, const std::string& compositionName) {
+    bool result = false;
+
+    ShutdownMovie();
+    gMovieFilePath = filePath;
+
+    ResourcesManager::Instance().Initialize();
+
+    if (gMovie.LoadFromFile(gMovieFilePath, gLicenseHash)) {
+        gComposition = compositionName.empty() ? gMovie.OpenDefaultComposition() : gMovie.OpenComposition(compositionName);
+        if (gComposition) {
+            MyLog << "Composition \"" << gComposition->GetName() << "\" loaded successfully" << MyEndl;
+            MyLog << " Duration: " << gComposition->GetDuration() << " seconds" << MyEndl;
+            gComposition->SetLoop(gToLoopPlay);
+            gComposition->Play();
+
+            result = true;
+        } else {
+            MyLog << "Failed to open the default composition" << MyEndl;
+        }
+    } else {
+        MyLog << "Failed to load the movie" << MyEndl;
+    }
+
+    return result;
+}
+
+void DoUI(UI& ui) {
     const ImGuiWindowFlags kPanelFlags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;// | ImGuiWindowFlags_NoCollapse;
 
     const float panelWidth = 200.0f;
     const float panelHeight = 200.0f;
+    float nextY = 0.0f;
 
-    ImGui::BeginMainMenuBar();
+    std::string compositionToPlay;
+    bool openNewMovie = false;
+
+    ImGui::SetNextWindowPos(ImVec2(0.0f, nextY));
+    ImGui::SetNextWindowSize(ImVec2(panelWidth, 0.0f));
+    ImGui::Begin("Movie:", nullptr, kPanelFlags);
     {
-        if (ImGui::BeginMenu("File")) {
-            if (ImGui::MenuItem("Open...")) {
-                nfdchar_t* outPath = nullptr;
-                if (NFD_OKAY == NFD_OpenDialog("aem", nullptr, &outPath)) {
-                    ui.fileToOpen = outPath;
-                    free(outPath);
-                }
+        char moviePath[1024] = { 0 };
+        char licenseHash[1024] = { 0 };
+        char compositionName[1024] = { 0 };
+
+        if (!gMovieFilePath.empty()) {
+            memcpy(moviePath, gMovieFilePath.c_str(), gMovieFilePath.length());
+        }
+        if (!gLicenseHash.empty()) {
+            memcpy(licenseHash, gLicenseHash.c_str(), gLicenseHash.length());
+        }
+        if (!compositionToPlay.empty()) {
+            memcpy(compositionName, compositionToPlay.c_str(), compositionToPlay.length());
+        }
+
+        ImGui::Text("Movie file path:");
+        if (ImGui::InputText("##FilePath", moviePath, sizeof(moviePath) - 1)) {
+            gMovieFilePath = moviePath;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("...")) {
+            nfdchar_t* outPath = nullptr;
+            if (NFD_OKAY == NFD_OpenDialog("aem", nullptr, &outPath)) {
+                gMovieFilePath = outPath;
+                free(outPath);
             }
+        }
 
-            ImGui::Separator();
+        ImGui::Text("Licence hash:");
+        if (ImGui::InputText("##LicenseHash", licenseHash, sizeof(licenseHash) - 1)) {
+            gLicenseHash = licenseHash;
+        }
 
-            if (ImGui::MenuItem("Exit")) {
-                ui.shouldExit = true;
-            }
+        ImGui::Text("Composition name:");
+        if (ImGui::InputText("##CompositionName", compositionName, sizeof(compositionName) - 1)) {
+            compositionToPlay = compositionName;
+        }
+        if (ImGui::IsItemHovered()) {
+            ImGui::SetTooltip("Leave it empty to play default composition");
+        }
 
-            ImGui::EndMenu();
+        ImGui::NewLine();
+        if (ImGui::Button("Play the movie !")) {
+            openNewMovie = true;
         }
     }
-    ImGui::EndMainMenuBar();
+    ImGui::End();
 
-    float nextY = 20.0f;
-
+    if (openNewMovie && !gMovieFilePath.empty() && !gLicenseHash.empty()) {
+        ReloadMovie(gMovieFilePath, compositionToPlay);
+    }
 
     ImGui::SetNextWindowPos(ImVec2(static_cast<float>(kWindowWidth) - panelWidth, nextY));
     ImGui::SetNextWindowSize(ImVec2(panelWidth, 0.0f));
-    ImGui::Begin("Viewer:", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+    ImGui::Begin("Viewer:", nullptr, kPanelFlags);
     {
         ImGui::Text("%.1f FPS (%.3f ms)", ImGui::GetIO().Framerate, 1000.0f / ImGui::GetIO().Framerate);
         ImGui::Checkbox("Draw normal", &ui.showNormal);
@@ -77,106 +153,104 @@ void DoUI(UI& ui, Composition* composition) {
     nextY += ImGui::GetWindowHeight();
     ImGui::End();
 
-    ImGui::SetNextWindowPos(ImVec2(static_cast<float>(kWindowWidth) - panelWidth, nextY));
-    ImGui::SetNextWindowSize(ImVec2(panelWidth, panelHeight));
-    ImGui::Begin("Movie info", nullptr, kPanelFlags);
-    {
-        const float duration = composition->GetDuration();
-        const float playTime = composition->GetCurrentPlayTime();
-        const float playPos = playTime / duration;
+    if (gComposition) {
+        ImGui::SetNextWindowPos(ImVec2(static_cast<float>(kWindowWidth) - panelWidth, nextY));
+        ImGui::SetNextWindowSize(ImVec2(panelWidth, panelHeight));
+        ImGui::Begin("Movie info:", nullptr, kPanelFlags);
+        {
+            const float duration = gComposition->GetDuration();
+            const float playTime = gComposition->GetCurrentPlayTime();
+            const float playPos = playTime / duration;
 
-        ImGui::Text("Composition: %s", composition->GetName().c_str());
-        ImGui::Text("Duration: %0.2fs", duration);
-        ImGui::Text("Play time: %0.2fs", playTime);
+            ImGui::Text("Composition: %s", gComposition->GetName().c_str());
+            ImGui::Text("Duration: %0.2fs", duration);
+            ImGui::Text("Play time: %0.2fs", playTime);
 
-        ImGui::ProgressBar(playPos);
+            ImGui::ProgressBar(playPos);
 
-        if (ImGui::Button("Play")) {
-            composition->Play();
+            if (ImGui::Button("Play")) {
+                gComposition->Play();
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Pause")) {
+                gComposition->Pause();
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Stop")) {
+                gComposition->Stop();
+            }
+
+            bool loopComposition = gComposition->IsLooped();
+            ImGui::Checkbox("Loop composition", &loopComposition);
+            if (loopComposition != gComposition->IsLooped()) {
+                gComposition->SetLoop(loopComposition);
+            }
         }
-        ImGui::SameLine();
-        if (ImGui::Button("Pause")) {
-            composition->Pause();
-        }
-        ImGui::SameLine();
-        if (ImGui::Button("Stop")) {
-            composition->Stop();
-        }
+        nextY += ImGui::GetWindowHeight();
+        ImGui::End();
 
-        bool loopComposition = composition->IsLooped();
-        ImGui::Checkbox("Loop composition", &loopComposition);
-        if (loopComposition != composition->IsLooped()) {
-            composition->SetLoop(loopComposition);
+        const size_t numSubCompositions = gComposition->GetNumSubCompositions();
+        if (numSubCompositions) {
+            ImGui::SetNextWindowPos(ImVec2(static_cast<float>(kWindowWidth) - panelWidth, nextY));
+            ImGui::SetNextWindowSize(ImVec2(panelWidth, panelHeight));
+            ImGui::Begin("Sub compositions:", nullptr, kPanelFlags);
+            {
+                for (size_t i = 0; i < numSubCompositions; ++i) {
+                    std::string guiID = std::to_string(i);
+                    std::string fullName = guiID + ") " + gComposition->GetSubCompositionName(i) + ":";
+                    ImGui::Text(fullName.c_str());
+
+                    std::string btnPlayNameAndId = std::string("Play##") + guiID;
+                    std::string btnPauseNameAndId = std::string("Pause##") + guiID;
+                    std::string btnStopNameAndId = std::string("Stop##") + guiID;
+                    std::string btnRewindNameAndId = std::string("Rewind##") + guiID;
+
+                    if (ImGui::Button(btnPlayNameAndId.c_str())) {
+                        gComposition->PlaySubComposition(i);
+                    }
+                    ImGui::SameLine();
+                    if (ImGui::Button(btnPauseNameAndId.c_str())) {
+                        gComposition->PauseSubComposition(i);
+                    }
+                    ImGui::SameLine();
+                    if (ImGui::Button(btnStopNameAndId.c_str())) {
+                        gComposition->StopSubComposition(i);
+                    }
+                    ImGui::SameLine();
+                    if (ImGui::Button(btnRewindNameAndId.c_str())) {
+                        gComposition->SetTimeSubComposition(i, 0.0f);
+                    }
+
+                    std::string chkLoopNameAndId = std::string("Loop play##") + guiID;
+
+                    bool loopPlay = gComposition->IsLoopedSubComposition(i);
+                    ImGui::Checkbox(chkLoopNameAndId.c_str(), &loopPlay);
+                    if (loopPlay != gComposition->IsLoopedSubComposition(i)) {
+                        gComposition->SetLoopSubComposition(i, loopPlay);
+                    }
+
+                    ImGui::NewLine();
+                }
+            }
+            ImGui::End();
         }
     }
-    nextY += ImGui::GetWindowHeight();
-    ImGui::End();
-
-    ImGui::SetNextWindowPos(ImVec2(static_cast<float>(kWindowWidth) - panelWidth, nextY));
-    ImGui::SetNextWindowSize(ImVec2(panelWidth, panelHeight));
-    ImGui::Begin("Sub compositions", nullptr, kPanelFlags);
-    {
-        const size_t numSubCompositions = composition->GetNumSubCompositions();
-        for (size_t i = 0; i < numSubCompositions; ++i) {
-            std::string guiID = std::to_string(i);
-            std::string fullName = guiID + ") " + composition->GetSubCompositionName(i) + ":";
-            ImGui::Text(fullName.c_str());
-
-            std::string btnPlayNameAndId = std::string("Play##") + guiID;
-            std::string btnPauseNameAndId = std::string("Pause##") + guiID;
-            std::string btnStopNameAndId = std::string("Stop##") + guiID;
-            std::string btnRewindNameAndId = std::string("Rewind##") + guiID;
-
-            if (ImGui::Button(btnPlayNameAndId.c_str())) {
-                composition->PlaySubComposition(i);
-            }
-            ImGui::SameLine();
-            if (ImGui::Button(btnPauseNameAndId.c_str())) {
-                composition->PauseSubComposition(i);
-            }
-            ImGui::SameLine();
-            if (ImGui::Button(btnStopNameAndId.c_str())) {
-                composition->StopSubComposition(i);
-            }
-            ImGui::SameLine();
-            if (ImGui::Button(btnRewindNameAndId.c_str())) {
-                composition->SetTimeSubComposition(i, 0.0f);
-            }
-
-            std::string chkLoopNameAndId = std::string("Loop play##") + guiID;
-
-            bool loopPlay = composition->IsLoopedSubComposition(i);
-            ImGui::Checkbox(chkLoopNameAndId.c_str(), &loopPlay);
-            if (loopPlay != composition->IsLoopedSubComposition(i)) {
-                composition->SetLoopSubComposition(i, loopPlay);
-            }
-
-            ImGui::NewLine();
-        }
-    }
-    ImGui::End();
 }
 
 int main(int argc, char** argv) {
-    std::string pathToFile;
     std::string compositionName;
     std::string isLooped;
     std::string licenseHash;
 
-    bool toLoop = false;
-
-    if(argc != 5) {
-        std::cout << "Usage:  viewer.exe path_to_file compositions_name is_looped hash" << std::endl;
-        return -1;
-    } else {
-        pathToFile      = argv[1];
+    if(argc == 5) {
+        gMovieFilePath  = argv[1];
         compositionName = argv[2];
         isLooped        = argv[3];
-        licenseHash     = argv[4];
+        gLicenseHash    = argv[4];
 
         std::transform(isLooped.begin(), isLooped.end(), isLooped.begin(), [](char c) { return static_cast<char>(std::tolower(c)); });
 
-        toLoop = (isLooped == "true" || isLooped == "1" || isLooped == "yes");
+        gToLoopPlay = (isLooped == "true" || isLooped == "1" || isLooped == "yes");
     }
 
     if (!glfwInit()) {
@@ -219,95 +293,58 @@ int main(int argc, char** argv) {
     ImGui_ImplGlfwGL3_Init(window, false);
     ImGui::StyleColorsClassic();
 
-    Movie movie;
-    if(movie.LoadFromFile(pathToFile, licenseHash)) {
-        MyLog << MyEndl << MyEndl << MyEndl;
-        MyLog << "Loaded movie ver. " << movie.GetVersion() << MyEndl;
+    ResourcesManager::Instance().Initialize();
 
-        Composition* composition = movie.OpenComposition(compositionName);
-        if (composition) {
-            MyLog << "Composition \"" << composition->GetName() << "\" loaded successfully" << MyEndl;
-            MyLog << " Duration: " << composition->GetDuration() << " seconds" << MyEndl;
+    if (!gMovieFilePath.empty()) {
+        ReloadMovie(gMovieFilePath, compositionName);
+    }
 
-            composition->SetLoop(toLoop);
-            composition->Play();
+    glViewport(0, 0, static_cast<GLint>(kWindowWidth), static_cast<GLint>(kWindowHeight));
+    glClearColor(0.412f, 0.796f, 1.0f, 1.0f);
 
-            glViewport(0, 0, static_cast<GLint>(kWindowWidth), static_cast<GLint>(kWindowHeight));
-            glClearColor(0.412f, 0.796f, 1.0f, 1.0f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-            glEnable(GL_BLEND);
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    float timeLast = GetCurrentTimeSeconds();
+    while (!glfwWindowShouldClose(window) && !ui.shouldExit) {
+        glfwPollEvents();
 
-            ResourcesManager::Instance().Initialize();
+        glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-            float timeLast = GetCurrentTimeSeconds();
-            while (!glfwWindowShouldClose(window) && !ui.shouldExit) {
-                glfwPollEvents();
+        const float timeNow = GetCurrentTimeSeconds();
+        const float dt = timeNow - timeLast;
+        timeLast = timeNow;
 
-                glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        ImGui_ImplGlfwGL3_NewFrame();
 
-                const float timeNow = GetCurrentTimeSeconds();
-                const float dt = timeNow - timeLast;
-                timeLast = timeNow;
-
-                ImGui_ImplGlfwGL3_NewFrame();
-
-                if (composition->IsPlaying()) {
-                    composition->Update(dt);
-                }
-
-                if (ui.showNormal || ui.showWireframe) {
-                    Composition::DrawMode drawMode;
-                    if (ui.showNormal && ui.showWireframe) {
-                        drawMode = Composition::DrawMode::SolidWithWireOverlay;
-                    } else if (ui.showWireframe) {
-                        drawMode = Composition::DrawMode::Wireframe;
-                    } else {
-                        drawMode = Composition::DrawMode::Solid;
-                    }
-                    composition->Draw(drawMode);
-                }
-
-                DoUI(ui, composition);
-
-                ImGui::Render();
-                ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
-
-                glfwSwapBuffers(window);
-
-                // we want to open the file
-                if (!ui.fileToOpen.empty()) {
-                    movie.CloseComposition(composition);
-
-                    if (movie.LoadFromFile(ui.fileToOpen, licenseHash)) {
-                        composition = movie.OpenDefaultComposition();
-                        if (composition) {
-                            MyLog << "Composition \"" << composition->GetName() << "\" loaded successfully" << MyEndl;
-                            MyLog << " Duration: " << composition->GetDuration() << " seconds" << MyEndl;
-                            composition->SetLoop(toLoop);
-                            composition->Play();
-                        } else {
-                            MyLog << "Failed to open the default composition" << MyEndl;
-                        }
-                    } else {
-                        MyLog << "Failed to load the movie" << MyEndl;
-                        break;
-                    }
-
-                    ui.fileToOpen.clear();
-                }
+        if (gComposition) {
+            if (gComposition->IsPlaying()) {
+                gComposition->Update(dt);
             }
 
-            movie.CloseComposition(composition);
-            ResourcesManager::Instance().Shutdown();
-        } else {
-            MyLog << "Failed to open the composition" << MyEndl;
+            if (ui.showNormal || ui.showWireframe) {
+                Composition::DrawMode drawMode;
+                if (ui.showNormal && ui.showWireframe) {
+                    drawMode = Composition::DrawMode::SolidWithWireOverlay;
+                } else if (ui.showWireframe) {
+                    drawMode = Composition::DrawMode::Wireframe;
+                } else {
+                    drawMode = Composition::DrawMode::Solid;
+                }
+
+                gComposition->Draw(drawMode);
+            }
         }
 
-        movie.Close();
-    } else {
-        MyLog << "Failed to load the movie" << MyEndl;
+        DoUI(ui);
+
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
+
+    ShutdownMovie();
 
     ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();
